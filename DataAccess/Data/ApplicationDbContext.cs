4d809d0736cd50d3a72c09e38433bcfd6965a715
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-15 12:43:42 +0200

    Create ApplicationDbContext

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
