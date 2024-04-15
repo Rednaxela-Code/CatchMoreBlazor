857c29cd08d95a8c4ab04afdb12a6e022e52d291
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-15 17:05:28 +0200

    Built test for Repository Remove() function.

--- Tests/RepositoryTests/RepositoryTest.cs
+++ Tests/RepositoryTests/RepositoryTest.cs
@@ -112,5 +112,40 @@ namespace Tests.RepositoryTests
             //Assert
             result.Should().BeNullOrEmpty();
         }
+
+        [Fact]
+        public void RemoveShouldDeleteEntity_WhenEntityExists()
+        {
+            //Arrange
+            var dbContextMock = new Mock<IDbContext>();
+            var expectedEntityOne = new Session { Id = 1, SessionName = "Test Session 1", Date = new DateOnly(2024, 3, 2), Latitude = 12, Longitude = 22 };
+            var expectedEntityTwo = new Session { Id = 2, SessionName = "Test Session 2", Date = new DateOnly(2023, 8, 23), Latitude = 21, Longitude = 2 };
+            var expectedEntityThree = new Session { Id = 3, SessionName = "Test Session 3", Date = new DateOnly(2023, 6, 12), Latitude = 5, Longitude = 54 };
+            var entities = new List<Session> { expectedEntityOne, expectedEntityTwo, expectedEntityThree }.AsQueryable();
+
+            var dbSetMock = new Mock<DbSet<Session>>();
+            dbSetMock.As<IQueryable<Session>>().Setup(m => m.Provider).Returns(entities.Provider);
+            dbSetMock.As<IQueryable<Session>>().Setup(m => m.Expression).Returns(entities.Expression);
+            dbSetMock.As<IQueryable<Session>>().Setup(m => m.ElementType).Returns(entities.ElementType);
+            dbSetMock.As<IQueryable<Session>>().Setup(m => m.GetEnumerator()).Returns(entities.GetEnumerator());
+
+            dbSetMock.Setup(m => m.Remove(It.IsAny<Session>())).Callback<Session>((entity) => entities = entities.Where(e => e.Id != entity.Id).AsQueryable());
+            dbContextMock.Setup(m => m.Set<Session>()).Returns(dbSetMock.Object);
+
+            var repository = new Repository<Session>(dbContextMock.Object);
+
+            //Act
+            repository.Remove(expectedEntityOne);
+
+            //Assert
+            /*
+            Ensure Set<Session>() was called once.
+            Ensure Remove() was called once.
+            Ensure Remove() was called with the correct object type.
+             */
+            dbContextMock.Verify(m => m.Set<Session>(), Times.Once);
+            dbSetMock.Verify(m => m.Remove(It.IsAny<Session>()), Times.Once);
+            dbSetMock.Verify(m => m.Remove(It.Is<Session>(e => e.Id == 1)), Times.Once);
+        }
     }
 }
